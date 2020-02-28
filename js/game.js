47e32d7d29205065e84fba7c9310a964cc8d9957
soloviev330 <60703672+soloviev330@users.noreply.github.com>
2020-02-29 01:44:10 +0300

    Add files via upload

---
+++ js/game.js
@@ -0,0 +1,65 @@
+const numDivs = 36;
+const maxHits = 10;
+
+let hits = 0;
+let firstHitTime = 0;
+
+function round() {
+  $(".game-field").removeClass("d-none");
+  $("div").removeClass("target");
+  $("div").removeClass("miss");
+  // FIXME: надо бы убрать "target" прежде чем искать новый
+
+  let divSelector = randomDivId();
+  $(divSelector).addClass("target");
+  $(divSelector).text(hits);
+  // TODO: помечать target текущим номером
+  firstHitTime = getTimestamp();
+  // FIXME: тут надо определять при первом клике firstHitTime
+
+  if (hits === maxHits) {
+    endGame();
+  }
+}
+
+function endGame() {
+  $(".game-field").addClass("d-none");
+  // FIXME: спрятать игровое поле сначала
+
+  let totalPlayedMillis = getTimestamp() - firstHitTime;
+  let totalPlayedSeconds = Number(totalPlayedMillis/1000).toPrecision(3);
+  $("#total-time-played").text(totalPlayedSeconds);
+
+  $("#win-message").removeClass("d-none");
+}
+
+function handleClick(event) {
+  // FIXME: убирать текст со старых таргетов. Кажется есть .text?
+  if ($(event.target).hasClass("target")) {
+    $(event.target).text("")
+
+    hits = hits + 1;
+    round();
+
+  }
+    else
+    $(event.target).addClass("miss");
+  // TODO: как-то отмечать если мы промахнулись? См CSS класс .miss
+}
+
+function init() {
+$(".game-field").addClass("d-none");
+  $("#button-load").click(function() {
+    $("#button-reload").removeClass("d-none");
+    $("#button-load").addClass("d-none");
+  // TODO: заказчик просил отдельную кнопку, запускающую игру а не просто по загрузке
+    round();
+  });
+
+  $(".game-field").click(handleClick);
+  $("#button-reload").click(function() {
+    location.reload();
+  });
+}
+
+$(document).ready(init);
